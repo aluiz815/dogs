dd4c45d76b7a0939440e7b1a6e6169a543cdf4c9
Andre Luiz <aluiz815@gmail.com>
2021-01-02 11:59:46 -0300

    Header

---
+++ src/Components/Home/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './home.module.css';
+
+function Home() {
+  return <div>home</div>;
+}
+
+export default Home;
---
+++ src/Components/Login/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './login.module.css';
+
+function Login() {
+  return <div>login</div>;
+}
+
+export default Login;
